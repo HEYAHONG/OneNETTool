00f361d2333b237807c1c3e7300648c26c82c806
HEYAHONG <2229388563@qq.com>
2023-04-13 17:23:56 +0800

    添加部分OneNET代码

---
+++ components/OneNET/onenetcommon.c
@@ -0,0 +1,20 @@
+﻿#include "onenetcommon.h"
+#include "cJSON.h"
+
+static bool g_IsInit=false;
+static cJSON_Hooks g_cjsonhooks=
+{
+    .malloc_fn=OneNETMalloc,
+    .free_fn=OneNETFree
+};
+
+void OneNETCommonInit()
+{
+    if(!g_IsInit)
+    {
+        //只初始化一次
+        g_IsInit=true;
+        cJSON_InitHooks(&g_cjsonhooks);
+    }
+}
+
---
+++ components/OneNET/onenetcommon.h
@@ -0,0 +1,59 @@
+﻿#ifndef ONENETCOMMON_H
+#define ONENETCOMMON_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif // HAVE_CONFIG_H
+
+
+/*
+在嵌入式系统中，可通过定义HAVE_ONENET_CONFIG_H并使用onenet_config.h覆盖默认的定义。
+*/
+#ifdef HAVE_ONENET_CONFIG_H
+#include "onenet_config.h"
+#endif // HAVE_ONENET_CONFIG_H
+
+#include "stdlib.h"
+#include "string.h"
+#include "stdbool.h"
+#include "stdint.h"
+
+/*
+内存分配
+*/
+#ifndef OneNETMalloc
+#define OneNETMalloc malloc
+#endif // OneNETMalloc
+
+/*
+内存释放
+*/
+#ifndef OneNETFree
+#define OneNETFree free
+#endif // OneNETFree
+
+/*
+拷贝字符串
+*/
+#ifndef OneNETStrdup
+#define OneNETStrdup strdup
+#endif // OneNETStrdup
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+
+/** \brief Common初始化
+ *
+ *
+ */
+void OneNETCommonInit();
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+
+#endif // ONENETCOMMON_H
